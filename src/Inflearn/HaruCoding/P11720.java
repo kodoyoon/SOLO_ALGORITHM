699336a996d7d47b350eef9f089da1993958e597
doyoon <doyoun0527@naver.com>
2024-01-25 12:46:34 +0900

    P11720_숫자의 합

--- src/Inflearn/HaruCoding/P11720.java
+++ src/Inflearn/HaruCoding/P11720.java
@@ -12,7 +12,7 @@ public class P11720 {
         char[] cNum = sNum.toCharArray();
         int sum = 0;
         for(int i = 0; i<cNum.length; i++) {
-            sum += cNum[i] - '0';
+            sum += cNum[i]-'0';
         }
         System.out.println(sum);
     }
