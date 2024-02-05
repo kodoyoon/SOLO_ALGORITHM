e8997460516b7bb4d54dc295ff4f4e908e22fa01
doyoon <doyoun0527@naver.com>
2024-02-05 10:57:36 +0900

    P11720_숫자의 합 -  설명추가

--- src/Inflearn/HaruCoding/P11720.java
+++ src/Inflearn/HaruCoding/P11720.java
@@ -7,12 +7,12 @@ public class P11720 {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-
+    // 입력값을 String 변수 sNum 에 저장한 후 char[] 형 변수로 변환하기
         String sNum = sc.next();
         char[] cNum = sNum.toCharArray();
         int sum = 0;
         for(int i = 0; i<cNum.length; i++) {
-            sum += cNum[i]-'0';
+            sum += cNum[i]-'0'; //cNum[i] 를 정수형으로 변환하면서 sum 에 더하며 누적시키기
         }
         System.out.println(sum);
     }
