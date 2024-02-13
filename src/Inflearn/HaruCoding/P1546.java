c543e76d0c5f143bcb69678cbef884fde20dcfe5
doyoon <doyoun0527@naver.com>
2024-02-13 15:24:22 +0900

    P1526_평균

--- src/Inflearn/HaruCoding/P1546.java
+++ src/Inflearn/HaruCoding/P1546.java
@@ -7,13 +7,13 @@ public class P1546 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int A[] = new int[N];
-        for (int i = 0; i < N; i++) {
+        for(int i = 0; i<N; i++) {
             A[i] = sc.nextInt();
         }
         long sum = 0;
         long max = 0;
-        for (int i = 0; i < N; i++) {
-            if (A[i] > max) max = A[i];
+        for(int i = 0; i<N; i++) {
+            if(A[i] > max) max = A[i];
             sum = sum + A[i];
         }
         System.out.println(sum * 100.0 / max / N);
