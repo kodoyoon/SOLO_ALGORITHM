6c4513f2b7873d7d872946b7ef0595f998207ad6
doyoon <doyoun0527@naver.com>
2024-02-20 20:03:15 +0900

    P12891_DNA 비밀번호

---
+++ src/Inflearn/HaruCoding/P12891.java
@@ -0,0 +1,22 @@
+package Inflearn.HaruCoding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P12891 {
+    static int checkArr[];
+    static int myArr[];
+    static int checkSecret;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        int Result = 0;
+        char A[] = new char[S];
+        checkArr = new int[4];
+        myArr = new int[4];
+    }
+}
