e2c0d88c5c050803a1ccfcc93eec4f5d36185960
doyoon <doyoun0527@naver.com>
2024-02-21 10:13:41 +0900

    P12891_DNA 비밀번호

--- src/Inflearn/HaruCoding/P12891.java
+++ src/Inflearn/HaruCoding/P12891.java
@@ -18,5 +18,13 @@ public class P12891 {
         char A[] = new char[S];
         checkArr = new int[4];
         myArr = new int[4];
+        checkSecret = 0;
+        A = bf.readLine().toCharArray();
+        st = new StringTokenizer(bf.readLine());
+        for(int i = 0; i< 4; i++) {
+            checkArr[i] = Integer.parseInt(st.nextToken());
+            if(checkArr[i] == 0)
+                checkSecret++;
+        }
     }
 }
